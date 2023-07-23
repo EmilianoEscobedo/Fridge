e132d9205e813e511cd6bc669b827089d492ee56
DiegoHaczek <diegohaczek1996@hotmail.com>
2023-07-23 20:19:28 -0300

    SP1-FR25: impl rekognition service: extract info from ticket

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -1,6 +1,8 @@
 
+import service.RekognitionService;
 import service.impl.IngredientServiceImpl;
 import service.impl.RecipeServiceImpl;
+import service.impl.RekognitionServiceImpl;
 import service.impl.UserServiceImpl;
 
 import javax.persistence.EntityManagerFactory;
@@ -12,9 +14,12 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
 
-        Endpoint.publish("http://localhost:8080/users", new UserServiceImpl());
+        /*Endpoint.publish("http://localhost:8080/users", new UserServiceImpl());
         Endpoint.publish("http://localhost:8080/ingredients", new IngredientServiceImpl());
-        Endpoint.publish("http://localhost:8080/recipes", new RecipeServiceImpl());
+        Endpoint.publish("http://localhost:8080/recipes", new RecipeServiceImpl());*/
+
+        RekognitionServiceImpl rekognition = new RekognitionServiceImpl();
+        rekognition.handleRequest(rekognition.mockRequest());
 
         emf.close();
     }
